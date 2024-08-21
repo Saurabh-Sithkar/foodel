024c747696260a3e5eb0b7a49e5d5bfb1de4f96b
Saurabh-Sithkar <pacific.learning0103@gmail.com>
2024-08-21 20:17:59 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,10 @@ app.get("/",(req,res)=>{
     res.send("API Working")
 })
 
-app.listen(port,()=>{
-    console.log(`Server started on http://localhost:${port}`)
-})
+// app.listen(port,()=>{
+//     console.log(`Server started on http://localhost:${port}`)
+// })
 
+export default app;
 
 //mongodb+srv://sithkar03:ranu@cluster0.x6hphl2.mongodb.net/?
